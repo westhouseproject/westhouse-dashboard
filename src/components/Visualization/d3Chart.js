914b21f99f5db45f6168355be66b8b3b93a9e3ad
Salehen Shovon Rahman <sal@linux.com>
2015-06-26 15:15:11 -0700

    Even more refactoring.

--- src/components/Visualization/d3Chart.js
+++ src/components/Visualization/d3Chart.js
@@ -1,54 +1,54 @@
 import d3 from 'd3';
 
 export function create(el, props, data) {
-    const margin = {top: 20, right: 20, bottom: 30, left: 50};
-    const width = 960 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+  const margin = {top: 20, right: 20, bottom: 30, left: 50};
+  const width = 960 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
 
-    const x = d3.time.scale()
-        .range([0, width]);
+  const x = d3.time.scale()
+      .range([0, width]);
 
-    const y = d3.scale.linear()
-        .range([height, 0]);
+  const y = d3.scale.linear()
+      .range([height, 0]);
 
-    const xAxis = d3.svg.axis()
-        .scale(x)
-        .orient('bottom');
+  const xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
 
-    const yAxis = d3.svg.axis()
-        .scale(y)
-        .orient('left');
+  const yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
 
-    const line = d3.svg.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.value); });
+  const line = d3.svg.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); });
 
-    const svg = d3.select(el).append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+  const svg = d3.select(el).append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain(d3.extent(data, function(d) { return d.value; }));
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.value; }));
 
-    svg.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis);
+  svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis);
 
-    svg.append('g')
-        .attr('class', 'y axis')
-        .call(yAxis)
-      .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 6)
-        .attr('dy', '.71em')
-        .style('text-anchor', 'end')
-        .text('Price ($)');
+  svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
+    .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end')
+      .text('Price ($)');
 
-    svg.append('path')
-        .datum(data)
-        .attr('class', 'line')
-        .attr('d', line);
+  svg.append('path')
+      .datum(data)
+      .attr('class', 'line')
+      .attr('d', line);
 }
